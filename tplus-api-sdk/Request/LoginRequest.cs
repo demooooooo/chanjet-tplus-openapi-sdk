4b359d0a91d17e595b019358462d5348347e363c
木木 <1518915457@qq.com>
2020-10-25 18:01:32 +0800

    添加登录接口

---
+++ tplus-api-sdk/Request/LoginRequest.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TPlus.Api.Response;
+
+namespace TPlus.Api.Request
+{
+    public class LoginRequest : IRequest<LoginResponse>
+    {
+        public string GetResourceMethod()
+        {
+            return "collaborationapp/GetRealNameTPlusToken?IsFree=1";
+        }
+
+        [JsonProperty("userName")]
+        public string UserName { get; set; }
+
+        [JsonProperty("password")]
+        public string Password { get; set; }
+
+        [JsonProperty("accNum")]
+        public string AccNum { get; set; }
+    }
+}
---
+++ tplus-api-sdk/Response/LoginResponse.cs
@@ -0,0 +1,21 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TPlus.Api.Response
+{
+    public class LoginResponse : TPlusResponse
+    {
+        [JsonProperty("result")]
+        public bool Result { get; set; }
+
+        [JsonProperty("access_token")]
+        public string AccessToken { get; set; }
+
+        [JsonProperty("sid")]
+        public string Sid { get; set; }
+    }
+}
