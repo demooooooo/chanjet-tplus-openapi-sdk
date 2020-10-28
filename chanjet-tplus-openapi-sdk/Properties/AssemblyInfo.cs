3c76c29a94abddf9915bc0a36db7fd07f426b159
木木 <1518915457@qq.com>
2020-10-28 20:14:16 +0800

    修改项目名称

--- chanjet-tplus-openapi-sdk/Properties/AssemblyInfo.cs
+++ chanjet-tplus-openapi-sdk/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // 有关程序集的一般信息由以下
 // 控制。更改这些特性值可修改
 // 与程序集关联的信息。
-[assembly: AssemblyTitle("tplus-api-sdk")]
+[assembly: AssemblyTitle("chanjet-tplus-openapi-sdk")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("tplus-api-sdk")]
+[assembly: AssemblyProduct("chanjet-tplus-openapi-sdk")]
 [assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
