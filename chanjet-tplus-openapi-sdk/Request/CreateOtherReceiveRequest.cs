6b08ba0d675d80200827a2bb86331b21a8dc39bb
木木 <1518915457@qq.com>
2020-11-08 12:06:53 +0800

    添加其他入库单创建接口

---
+++ chanjet-tplus-openapi-sdk/Request/CreateOtherReceiveRequest.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TPlus.Api.Domain;
+using TPlus.Api.Response;
+
+namespace TPlus.Api.Request
+{
+    public class CreateOtherReceiveRequest : IRequest<CreateOtherReceiveResponse>
+    {
+        public string GetResourceMethod()
+        {
+            return "otherReceive/Create";
+        }
+
+        [JsonProperty("dto")]
+        public RDRecordDTO OtherReceive { get; set; }
+    }
+}
