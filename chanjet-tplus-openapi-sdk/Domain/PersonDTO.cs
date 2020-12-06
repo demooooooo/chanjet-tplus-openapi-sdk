2d6ce365a2b176daea340e8a161fe7e3f7e72c24
木木 <1518915457@qq.com>
2020-12-06 17:50:27 +0800

    添加员工查询接口

--- chanjet-tplus-openapi-sdk/Domain/PersonDTO.cs
+++ chanjet-tplus-openapi-sdk/Domain/PersonDTO.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,28 @@ namespace TPlus.Api.Domain
 {
     public class PersonDTO : DTO
     {
+        /// <summary>
+        /// 出生日期
+        /// </summary>
+        [JsonProperty("Birthday")]
+        public DateTime? Birthday { get; set; }
+
+        /// <summary>
+        /// 所属部门
+        /// </summary>
+        [JsonProperty("Department")]
+        public DepartmentDTO Department { get; set; }
+
+        /// <summary>
+        /// 业务员
+        /// </summary>
+        [JsonProperty("IsSalesman")]
+        public bool? IsSalesman { get; set; }
+
+        /// <summary>
+        /// 手机号
+        /// </summary>
+        [JsonProperty("MobilePhoneNo")]
+        public string MobilePhoneNo { get; set; }
     }
 }
---
+++ chanjet-tplus-openapi-sdk/Request/Person/QueryPersonRequest.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TPlus.Api.Domain;
+using TPlus.Api.Response.Person;
+
+namespace TPlus.Api.Request.Person
+{
+    public class QueryPersonRequest : TPlusRequest<QueryPersonResponse>
+    {
+        public override string GetResourceMethod()
+        {
+            return "person/Query";
+        }
+
+        [JsonProperty("dto")]
+        public PersonDTO Param { get; set; }
+    }
+}
